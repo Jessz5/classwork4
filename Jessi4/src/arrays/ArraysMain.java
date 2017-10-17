2a2a1b9d25a617adc26219c9662bebd0de034700
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-10-17 11:44:32 -0400

    LCS

--- Jessi4/src/arrays/ArraysMain.java
+++ Jessi4/src/arrays/ArraysMain.java
@@ -8,15 +8,105 @@ public class ArraysMain {
 	private int[] intRay;
 
 	public ArraysMain() {
-		intRay = new int[100];
+		//int[] consecTest1 = {1,2,3,6,7,8,9,10,11,45,46,47};
+		//int[] consecTest2 = {10,11,12,13,14,15,6,7,8,9,10,11,45,46,47};
+		int[] randomRolls = new int[1000];
+		populate(randomRolls);
+		
+		//System.out.println("The longest sequence in the first test is " + longestConsecutiveSequence(consecTest1));
+		//System.out.println("The longest sequence in the second test is " + longestConsecutiveSequence(consecTest2));
+		//System.out.println("The longest sequence in the third test is " + longestConsecutiveSequence(randomRolls));
+		int[] result = longestConsecSeqAndPos(randomRolls);
+		System.out.println("The longest sequence of dice rolls is " + result[0] + " it happened on the " + (result[1]+1) + "th roll. Starting with a roll of "+randomRolls[result[1]]+".");
+		
+		//intRay = new int[100];
 		//populate(intRay);
 		//checkOccurences(intRay,3,18);
-		populate1ToN(intRay);
+		//populate1ToN(intRay);
 		//swap(intRay,0,1);
-		shuffle(intRay);
+		//shuffle(intRay);
+		//reverseOrder(intRay);
+		//frontToBack(intRay);
 		//Arrays is a Utility class  included in Java for formatting output
-		System.out.println(Arrays.toString(intRay));
+		//System.out.println(Arrays.toString(intRay));
 	}
+	
+	public int countLessThan(int[] arr, int n) {
+		int count = 0;
+		for(int vale: arr) {
+			if(value < n)count++;
+		}
+		return count;
+	}
+	
+	
+	//private void reverseOrder(int[]arr) {
+		//int[] newArr = new int[arr.length];
+		//for(int i = 0; i < arr.length; i++) {
+		//	newArr[i] = arr[arr.length - 1 -i];
+		//}
+		//return newArr;
+	//}
+	
+	private void reverseOrder(int[]arr) {
+		for(int i = 0; i < arr.length/2; i++) {
+			swap(arr,i,arr.length - 1 - i);
+		}
+	}
+
+	/**
+	 * remove the element at index zero, push every other element up by one. 1 to 0, 2 to 1, etc...
+	 * Put the element that was at zero at the end of arr
+	 * @param arr
+	 */
+	public void frontToBack(int[] intRay) {
+		int front = intRay[0];
+		for(int i = 0; i < intRay.length - 1; i++) {
+			swap(intRay, i, i+1);
+		}
+	}
+	
+	public void cycleThrough(int[] arr, int n) {
+		for(int i = 0; i < n; i++) {
+			frontToBack(arr);
+		}
+	}
+	
+	
+	
+	public int longestConsecutiveSequence(int[] arr) {
+		int count = 0;
+		int temp = 0;
+		for(int i = 0; i < arr.length; i++) {
+		  if(isConsecutive(arr,i)) {
+			  count ++;
+		  }
+		  else {
+			  if(temp < count) {
+				  temp = count;
+				  count = 1;
+			  }
+		  }
+		}
+		if(temp < count)
+			temp = count;
+		return count;
+	}
+	
+	public int[] longestConsecSeqAndPos(int[] arr) {
+		int[] data = new int[2];
+		
+		return data;
+	}
+	
+	public boolean isConsecutive(int[] arr, int pos) {
+		if(arr[pos+1] == arr[pos]+1) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
 	
 	private void shuffle(int[] arr) {
 		for(int i = 0; i < arr.length; i++) {
