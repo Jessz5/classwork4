4adc541d40f7006fe1c586a68178aa63e3ace25e
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-10-31 11:21:43 -0400

    10/31

---
+++ Jessi4/src/caveExplorer/NPC.java
@@ -0,0 +1,28 @@
+package caveExplorer;
+/**
+ * NON PLAYABLE CHARACTER 
+ * @author BT_1N3_05
+ *
+ */
+public class NPC {
+
+	public NPC() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean isActive() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void interact() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getInactiveDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
