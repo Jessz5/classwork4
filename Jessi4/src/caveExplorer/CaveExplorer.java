9e4a1b7280ff954c307941a7a3e5a5dca6c12824
BT_1N3_03 <BT_1N3_03@BT_1N3_03.bktech.local>
2017-11-01 11:45:20 -0400

    11/1

--- Jessi4/src/caveExplorer/CaveExplorer.java
+++ Jessi4/src/caveExplorer/CaveExplorer.java
@@ -9,6 +9,7 @@ public class CaveExplorer {
 	public static CaveRoom currentRoom;//changes based on how the user navigated 
 	public static Inventory inventory;//where all objects found in cave are kept
 	public static boolean playing = true;
+	public static NPC[] npcs;
  	
 	public static void main(String[] args) {
 		in = new Scanner(System.in);
@@ -20,6 +21,7 @@ public class CaveExplorer {
 
 	private static void startExploring() {
 		while(playing) {
+			npcActions();
 			print(inventory.getDescription());
 			print(currentRoom.getDescription());
 			print("What would like to do?");
@@ -27,6 +29,13 @@ public class CaveExplorer {
 			currentRoom.interpretInput(input);
 		}
 		
+	}
+
+	private static void npcActions() {
+		for(NPC n: npcs) {
+			n.act();
+		}
+		inventory.updateMap();
 	}
 
 	public static void print(String s) {
