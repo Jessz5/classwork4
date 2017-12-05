8d08222d595e9556d960bfd04304e0cd4081eaf0
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-12-05 10:53:43 -0500

    Fibonacci

--- Jessi4/src/fibonacci/Fibonacci.java
+++ Jessi4/src/fibonacci/Fibonacci.java
@@ -5,7 +5,7 @@ public class Fibonacci {
 
 	
 	public static int fibonacciSolution(int n) {
-		if(n == 1) {
+		if(n <= 1) {
 			return n;
 		}else {
 			return fibonacciSolution(n-1) + fibonacciSolution(n-2);
